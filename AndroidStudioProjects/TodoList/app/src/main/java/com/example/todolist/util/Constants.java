ded8f79ec0dc843cd96ff20034f24985cd47a2b4
NamanKhurana <khurananaman06@gmail.com>
2019-08-30 11:43:14 +0530

    initial commit

---
+++ AndroidStudioProjects/TodoList/app/src/main/java/com/example/todolist/util/Constants.java
@@ -0,0 +1,16 @@
+package com.example.todolist.util;
+
+public class Constants {
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "babylist";
+    public static final String TABLE_NAME = "baby_tbl";
+
+    //Table Columns
+    public static final String KEY_ID = "id";
+    public static final String KEY_BABY_ITEM = "baby_item";
+    public static final String KEY_QTY_NUMBER = "quantity_number";
+    public static final String KEY_COLOR = "color";
+    public static final String KEY_ITEM_SIZE = "size";
+    public static final String KEY_DATE_NAME = "date_added";
+
+}
